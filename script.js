bbf102c152670f5998d1e17f59ab33896d8cef09
Adarsh Gupta <adgupta183@gmail.com>
2023-04-29 02:09:41 +0530

    added files

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.element', {
+    strings: ['Student,', 'Front-End Developer,','Machine Learning Enthusiast.'],
+    typeSpeed: 35,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 35,
+    loop: true,
+    loopCount: Infinity,
+});
